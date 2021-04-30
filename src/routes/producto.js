996a5bb6e5e1ffe54dc3484f43ff61de40f71785
emmanuelmateo <emmanuelmateo@outlook.com>
2021-04-29 22:06:25 -0500

    limpiamos la rama

--- src/routes/producto.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as producto_controller from "../controllers/producto";
-import producto from "../models/producto";
-
-export const producto_router = Router();
-producto_router.route("/productos")
-    .post(producto_controller.crearProducto)
-    .get(producto_controller.listarProductos);
